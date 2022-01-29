082427327039e9cbf99af87be70d27888f07c6bc
Jagad R R <43224845+ZenthicMC@users.noreply.github.com>
2022-01-29 22:58:42 +0700

    Update functions.php
    Added Pagination For List Books

--- functions.php
+++ functions.php
@@ -12,4 +12,35 @@ function query($query) {
     }
     return $rows;
 }
+
+function pagination($dataPerHalaman) {
+    // configure pagination
+
+    global $conn;
+
+    // menghitung jumlah data
+    $jumlahData = count(query("SELECT * FROM books"));
+
+    // menghitung jumlah halaman dgn cara membulatkan angka ke bilangan bulat keatas
+    $jumlahHalaman = ceil($jumlahData / $dataPerHalaman);
+
+    // menghitung halaman aktif
+    $halamanAktif = (isset($_GET['page'])) ? $_GET['page'] : 1;
+    if($halamanAktif <= 0) {
+        $halamanAktif = 1;
+        echo "<script>window.location.href=books.php?page=$halamanAktif;</script>";
+    }
+    elseif($halamanAktif > $jumlahHalaman) {
+        $halamanAktif = $jumlahHalaman;
+        echo "<script>window.location.href=books.php?page=$halamanAktif;</script>";
+    }
+   
+    // menghitung data awal untuk setiap halaman
+    $dataAwal = ($dataPerHalaman * $halamanAktif) - $dataPerHalaman;
+
+    $sql = "SELECT * FROM books LIMIT $dataAwal,$dataPerHalaman";
+    $result = $conn->query($sql);
+
+    return array($result,$jumlahData,$dataAwal,$dataPerHalaman,$halamanAktif,$jumlahHalaman);
+}
 ?>
