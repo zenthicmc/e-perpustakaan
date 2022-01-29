0ba5d39ffab7e1ebd9d9e1f0e05b67944c5c4dc0
Jagad R R <43224845+ZenthicMC@users.noreply.github.com>
2022-01-29 16:11:46 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
-$servername = "sql213.epizy.com";
-$username = "epiz_30934002";
-$password = "XwTIosfv28Ez";
-$dbname = "epiz_30934002_perpus";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "perpus";
 ?>
